a18970a40b6f3e728bcaa134de27ec95682ff6b7
evelinaEvlogieva <79201591+evelinaEvlogieva@users.noreply.github.com>
2021-03-08 12:08:04 +0200

    Add files via upload

---
+++ 08. Generics/Scale.java
@@ -0,0 +1,19 @@
+public class Scale <T extends Comparable<T>>{
+    private T left;
+    private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier (){
+        if (this.left.compareTo(this.right) < 0){
+            return this.right;
+        } else if (this.left.compareTo(this.right) > 0 ){
+            return this.left;
+        }
+
+     return null;
+    }
+}
